05c0bbce78d7945d60bf07be23f2c3c6d2e13bcc
jamesicon <james.williams@iconsavingsplan.com>
2022-03-25 12:17:47 -0700

    For issue #14272

--- src/sscce.js
+++ src/sscce.js
@@ -24,13 +24,37 @@ module.exports = async function() {
     }
   });
 
-  const Foo = sequelize.define('Foo', { name: DataTypes.TEXT });
+  const Foo = sequelize.define('Foo', { username: DataTypes.STRING });
+
+  Foo.findByUsername = async (username) => {
+    let foo = await Foo.findOne({
+      where: {
+        username: sequelize.where(
+          sequelize.fn('LOWER', sequelize.col('username')),
+          '=',
+          sequelize.fn('LOWER', username)
+        ),
+      },
+    });
+    return foo;
+  };
+
+  const username1 = 'money'
+  const username2 = 'money$'
 
   const spy = sinon.spy();
   sequelize.afterBulkSync(() => spy());
   await sequelize.sync();
   expect(spy).to.have.been.called;
 
-  log(await Foo.create({ name: 'foo' }));
-  expect(await Foo.count()).to.equal(1);
+  log(await Foo.create({ username: username1 }));
+  log(await Foo.create({ username: username2 }));
+  expect(await Foo.count()).to.equal(2);
+  const foundFoo1 = await Foo.findByUsername(username1)
+  const foundFoo2 = await Foo.findByUsername(username2)
+  
+  // the model without a $ in username is returned 
+  expect(typeof foundFoo1).to.equal('object')
+  // this test fails because the $ sign is being escaped incorrectly
+  expect(foundFoo2).to.not.be.null
 };
